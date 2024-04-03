e0e379f8b1caa0f00e58441767a333ac53859f81
jean-hub23 <jecachusa.1996@hotmail.com>
2024-04-03 07:48:12 -0500

    primer commit

---
+++ app/src/main/java/com/example/horseapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.horseapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.horseapp.ui.theme.HorseAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HorseAppTheme {
+        Greeting("Android")
+    }
+}
